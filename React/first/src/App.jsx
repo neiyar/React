61cf85ba86325f7b7b0b7eaff6de2c4d8225b919
neiyar <subhani17786@gmail.com>
2025-06-25 18:48:20 +0530

    first commit

---
+++ React/first/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Test from "./Test"
+
+function App() {
+
+
+  return (
+   <div>
+    <h1>hello </h1>
+    <Test/>
+   </div>
+  )
+}
+
+export default App
---
+++ React/first/src/Test.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const Test=()=>{
+    const[count,setcount]=useState(0)
+
+    const sum=(a,b)=>{
+    setcount(a+b)
+     
+    }
+   
+
+    return(
+        <div>
+           <h1>Naiyer subhani</h1> 
+           <h2>g</h2>
+           <h1>{count}</h1>
+           <button onClick={()=>sum(3,5)}> sum</button>
+        </div>
+    )
+}
+
+export default Test;
